3c9692c2deb2a3fc48763efb23acee398032d57d
youngbad <jakubbadowski.jb@gmail.com>
2024-01-01 22:14:48 +0100

    scripts cleaning

--- 0_init/3_data.R
+++ 0_init/3_data.R
@@ -1,7 +1,9 @@
+#### Extract data ####
 shops <- readRDS("data/shops.RDS")
 sales <- readRDS("data/sales.RDS")
 population <- readRDS("data/population.RDS")
 
+#### Transform data ####
 sales$date <- sales$date %>% as.Date()
 
 containers <- tibble::tibble(container_type = unique(sales$container))
@@ -10,3 +12,4 @@ containers$img = c(
   paste0('<img src="images/plastic-bottle.png" width="30" height="22"/>', containers$container_type[2]),
   paste0('<img src="images/can.jpeg" width="30" height="22"/>', containers$container_type[3])
   )
+
--- data_processing.R
+++
@@ -1,3 +0,0 @@
-sales$cost <- sales$sales * (sample(75:95, size = nrow(sales), replace = TRUE)/100)
-saveRDS(sales, "sales.rds")
-
